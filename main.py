ebe0da8d02a62e262cb8cbec4560c5ab5b5e6bb9
cre92299 <99609258+cre92299@users.noreply.github.com>
2022-12-31 16:39:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,97 +3,75 @@ from datetime import date
 from collections import Counter
 
 actions = {
-    'action1':'It was terrible                                       1',
-    'action2':'It was pretty bad                                     2',                                     
-    'action3':'It was alright                                        3',
-    'action4':'It was pretty good, nothing special about it though   4',
-    'action5':'It was amazing!!!!!!!!!                               5',
-}   
-print('Hey. . ')
-sleep(0.7)
+    'action1': 'It was terrible                                       1',
+    'action2': 'It was pretty bad                                     2',                                     
+    'action3': 'It was alright                                        3',
+    'action4': 'It was pretty good, nothing special about it though   4',
+    'action5': 'It was amazing!!!!!!!!!                               5',
+}
 
-print('Soooooooo. . ..  . How was your day?')
-sleep(0.25)
-print(
-    "-----------------------------------------",
-    "\n",
-    actions["action1"],
-    "\n",
-    actions["action2"],
-    "\n",
-    actions["action3"],
-    "\n",
-    actions["action4"],
-    "\n",
-    actions["action5"],
-    "\n",
-    "-----------------------------------------",
-)
-actionask = input('Which of these sounds right?  : ')
-
-correctnumbs = ['1','2','3','4','5']
-
-if actionask not in correctnumbs:
-    print('Wrong number')
-    sleep(2)
-    exit()
-
-ensureity = str(input("Do you want to report this to 'report.txt' (Y - Yes, N - No) :"))
-
-
-if ensureity == 'Y':
-    with open('report.txt', 'a') as cass2:
-     cass2.write(f"\n {actionask} | {date.today()}")
-
-else:
-    pass
-with open('report.txt') as file:
-    reportfile = file.read()
-
-abcd = ''.join(reportfile).split()
-
-jj = abcd[0::3]
-
-aba = Counter(jj)
-
-mean123 = aba.get
-
-if actionask == '1':
+def ask_for_rating():
+    print('Hey. . ')
+    sleep(0.7)
+    print('Soooooooo. . ..  . How was your day?')
+    sleep(0.25)
     print(
-        "Do something you like, have fun, don't make one day bad turn into many"
+        "-----------------------------------------",
+        "\n",
+        actions["action1"],
+        "\n",
+        actions["action2"],
+        "\n",
+        actions["action3"],
+        "\n",
+        actions["action4"],
+        "\n",
+        actions["action5"],
+        "\n",
+        "-----------------------------------------",
     )
-    sleep(1)
-    print('The action 1 was reported', aba.get('1'), 'times')
-    sleep(3)
-elif actionask == '2':
-    print(
-        "Do something you like, make days like this uncommon go outside, make friends whatever the shit"
-        )
-    sleep(1)
-    print(" Do something you like, watch a movie, watch a tv show, learn more code, enjoy yourself" )
-    print('The action 2 was reported', aba.get('2'), 'times')
-    sleep(3)
-elif actionask == '3':
-    print(
-        "Not so bad, lets start making alright days, uncommon"
-    )
-    sleep(1)
-    print('The action 3 was reported', aba.get('3'), 'times')
 
-elif actionask == '4':
-    print(
-        "Progess, hope he didnt do anything too bad, but atleast nothing that bad happened today :)"
-    )
-    sleep(1)
-    print('The action 4 was reported', aba.get('4'), 'times')
-    sleep(3)
-elif actionask == '5':
-    print("Congrats, hope you did lots of fun stuff in your day :DDD")
-    sleep(1)
-    print('The action 5 was reported', aba.get('5'), 'times')
+def report_rating(rating):
+    with open('report.txt', 'a') as f:
+        f.write(f"\n {rating} | {date.today()}")
+
+def print_rating_stats():
+    with open('report.txt') as file:
+        report_file = file.read()
+    
+    ratings = ''.join(report_file).split()[0::3]
+    rating_counts = Counter(ratings)
+    
+    print(f"The action {rating} was reported {rating_counts.get(rating)} times")
+
+def print_response(rating):
+    if rating == '1':
+        print("make the rest of your day better")
+    elif rating == '2':
+        print("make the rest of your day count")
+    elif rating == '3':
+        print("Pretty good, enjoy the rest of your day")
+    elif rating == '4':
+        print("Nice, enjoy the rest of your day")
+    elif rating == '5':
+        print("Congrats, hope you did lots of fun stuff in your day :DDD")
+
+def main():
+    ask_for_rating()
+    rating = input('Which of these sounds right?  : ')
+    if rating not in ['1', '2', '3', '4', '5']:
+        print('Wrong number')
+        sleep(2)
+        exit()
+    ensure_report = input("Do you want to report this to 'report.txt' (Y - Yes, N - No) :")
+    if ensure_report == 'Y':
+        report_rating(rating)
+    print_response(rating)
+    print_rating_stats()
     sleep(3)
 
-
+if __name__ == "__main__":
+    main()
 
 
 
